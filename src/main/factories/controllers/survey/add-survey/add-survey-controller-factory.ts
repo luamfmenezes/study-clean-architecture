48aeb9978807bf0ec96191fe9887a20b5799b555
Luam F Menezes <luamfmenezes@gmail.com>
2021-02-22 20:29:49 -0300

    refactor: organize folders inside main/factories/usecases

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -2,7 +2,7 @@ import { Controller } from '../../../../../presentation/protocols';
 import { makeSurveyValidation } from './add-survey-validation-factory';
 import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory';
 import { AddSurveyController } from '../../../../../presentation/controllers/survey/add-survey/add-survey-controller';
-import { makeDbAddSurvey } from '../../../usecases/add-survey/db-add-account-factory';
+import { makeDbAddSurvey } from '../../../usecases/survey/add-survey/db-add-account-factory';
 
 export const makeAddSurveyController = (): Controller => {
   const validation = makeSurveyValidation();
