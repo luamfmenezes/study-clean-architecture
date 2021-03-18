55f302eb915ab4bafe7c3099387f9ec7ba934f7f
Luam F Menezes <luamfmenezes@gmail.com>
2021-03-18 19:49:52 -0300

    feat: insert no-cache controll on swagger api

--- src/main/config/config-swagger.ts
+++ src/main/config/config-swagger.ts
@@ -1,7 +1,8 @@
 import { Express } from 'express';
 import { serve, setup } from 'swagger-ui-express';
 import swaggerConfig from '../docs';
+import { noCache } from '../middlewares/no-cache';
 
 export default (app: Express): void => {
-  app.use('/api-docs', serve, setup(swaggerConfig));
+  app.use('/api-docs', noCache, serve, setup(swaggerConfig));
 };
