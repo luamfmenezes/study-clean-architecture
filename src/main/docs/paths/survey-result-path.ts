e5bafeeec30ff427b47c8c1f4725f3aa23c0e55b
Luam F Menezes <luamfmenezes@gmail.com>
2021-03-24 18:59:59 -0300

    docs: create load survey result api documentation

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -46,4 +46,42 @@ export const surveyResultPath = {
       },
     },
   },
+  get: {
+    security: [
+      {
+        apiKeyAuth: [],
+      },
+    ],
+    tags: ['Surveys'],
+    summary: 'API to list survey results',
+    parameters: {
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string',
+      },
+    },
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult',
+            },
+          },
+        },
+      },
+      403: {
+        $ref: '#/components/forbidden',
+      },
+      404: {
+        $ref: '#/components/notFound',
+      },
+      500: {
+        $ref: '#/components/serverError',
+      },
+    },
+  },
 };
